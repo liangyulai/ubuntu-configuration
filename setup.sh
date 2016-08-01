cff3a71f778d3f6669641c9aff8b7aef77aedef6
Yulai <liangyulai@hotmail.com>
2016-08-01 14:37:29 +0800

    setup zshrc

--- setup.sh
+++ setup.sh
@@ -5,11 +5,19 @@ timestamp() {
 }
 
 if [ -f ~/.gitconfig ]; then
-	echo "backup your old git configuration"
+	echo "backup your old git configuration: .gitconfig_$(timestamp)"
 	mv ~/.gitconfig ~/.gitconfig_$(timestamp)
 fi
 
 ln -s $PWD/gitconfig ~/.gitconfig
+
+
+if [ -f ~/.zshrc ]; then
+	echo "backup your old zsh configuration: .zshrc_$(timestamp)"
+	mv ~/.zshrc ~/.zshrc_$(timestamp)
+fi
+
+ln -s $PWD/zshrc ~/.zshrc
 	
 
 
